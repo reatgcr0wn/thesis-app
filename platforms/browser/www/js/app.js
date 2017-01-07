dbc3346a57685222025e833dc66d4dc3962cf596
reatgcr0wn <reatgcr0wn@gmail.com>
2017-01-07 16:24:14 +0900

    hobby

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -3,64 +3,15 @@
     // var app = angular.module('my-app', ['onsen.directives']);
     var app = ons.bootstrap();
     app.controller('AppController', function($scope) {
-        //スイッチチェック
-
         $scope.beacon = {};
 
         $scope.changeSwitch = function() {
-            // $scope.beacon = !$scope.beacon;
+
             console.log($scope.beacon.on);
-            if ($scope.beacon.on) { //on
-
-                var uuid = '00000000-0000-0000-0000-000000000000';
-                var identifier = 'advertisedBeacon';
-                var minor = 2000;
-                var major = 5;
-                var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
-                console.log(beaconRegion);
-
-                cordova.plugins.locationManager.requestWhenInUseAuthorization();
-
-                // The Delegate is optional
-                var delegate = new cordova.plugins.locationManager.Delegate();
-
-                // Event when advertising starts (there may be a short delay after the request)
-                // The property 'region' provides details of the broadcasting Beacon
-                delegate.peripheralManagerDidStartAdvertising = function(pluginResult) {
-                    console.log('peripheralManagerDidStartAdvertising: ' + JSON.stringify(pluginResult.region));
-                };
-                // Event when bluetooth transmission state changes
-                // If 'state' is not set to BluetoothManagerStatePoweredOn when advertising cannot start
-                delegate.peripheralManagerDidUpdateState = function(pluginResult) {
-                    console.log('peripheralManagerDidUpdateState: ' + pluginResult.state);
-                };
-
-                cordova.plugins.locationManager.setDelegate(delegate);
-
-                // Verify the platform supports transmitting as a beacon
-                cordova.plugins.locationManager.isAdvertisingAvailable()
-                    .then(function(isSupported) {
-                        if (isSupported) {
-                            console.log("Start advertising...");
-                            cordova.plugins.locationManager.startAdvertising(beaconRegion)
-                                .fail(conole.error)
-                                .done();
-                        } else {
-                            console.log("Advertising not supported");
-                        }
-                    })
-                    .fail(function(e) {
-                        console.error(e);
-                    })
-                    .done();
-            } else { //off
-                // alert('unchecked');
-                cordova.plugins.locationManager.stopAdvertising()
-                    .fail(function(e) {
-                        console.error(e);
-                    })
-                    .done();
-                console.log($scope.beacon.on);
+            if ($scope.beacon.on) {
+              startAdvertise();
+            } else {
+              stopAdvertising();
             }
         };
 
@@ -72,11 +23,105 @@
       $scope.changeGender = function() {
 
         console.log($scope.beaocn.gender);
-        $scope.beacon.on = true;
         console.log($scope.beacon.on);
       }
 
     });
 
+    app.controller('generationController', function($scope) {
+      //性別
+      $scope.changeGeneration = function() {
+
+        console.log($scope.beaocn.gender);
+        console.log($scope.beacon.on);
+      }
+
+    });
+
+    app.controller('testController', function($scope) {
+      $scope.hobbies = [
+        {id: 1, name: 'パソコン・インターネット'},
+        {id: 2, name: '旅行観光'},
+        {id: 3, name: '盆栽・ガーデニング・家庭菜園など'},
+        {id: 4, name: '映画鑑賞・DVD鑑賞'},
+        {id: 5, name: '旅行観光'},
+        {id: 6, name: 'ジョギング・ウォーキング'},
+        {id: 7, name: '写真撮影・映像撮影'},
+        {id: 8, name: '宝くじ・懸賞など'},
+        {id: 9, name: 'ドライブ'},
+        {id: 10, name: 'ゲーム'},
+        {id: 11, name: 'スポーツ観戦'},
+        {id: 12, name: '釣り'},
+        {id: 13, name: '編み物・手芸など'},
+        {id: 14, name: '登山トレッキングなど'},
+        {id: 15, name: '料理'},
+        {id: 16, name: '楽器演奏・バンドなど'},
+        {id: 17, name: '資格取得の学習'},
+        {id: 18, name: '絵画・陶芸・工芸など'},
+        {id: 19, name: '囲碁・将棋・麻雀など'},
+        {id: 20, name: 'カラオケ'},
+        {id: 21, name: 'その他'},
+      ];
+      console.log($scope.hobbies);
+      $scope.check = function() {
+        var checks = [];
+        angular.forEach($scope.hobbies, function(hobby) {
+          if (hobby.checked) checks.push(hobby.id);
+        });
+        console.log(checks);
+      };
+    });
 
 })();
+
+function startAdvertise() {
+      var uuid = '00000000-0000-0000-0000-000000000000';
+      var identifier = 'advertisedBeacon';
+      var minor = 2000;
+      var major = 5;
+      var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+      console.log(beaconRegion);
+
+      cordova.plugins.locationManager.requestWhenInUseAuthorization();
+
+      // The Delegate is optional
+      var delegate = new cordova.plugins.locationManager.Delegate();
+
+      // Event when advertising starts (there may be a short delay after the request)
+      // The property 'region' provides details of the broadcasting Beacon
+      delegate.peripheralManagerDidStartAdvertising = function(pluginResult) {
+          console.log('peripheralManagerDidStartAdvertising: ' + JSON.stringify(pluginResult.region));
+      };
+      // Event when bluetooth transmission state changes
+      // If 'state' is not set to BluetoothManagerStatePoweredOn when advertising cannot start
+      delegate.peripheralManagerDidUpdateState = function(pluginResult) {
+          console.log('peripheralManagerDidUpdateState: ' + pluginResult.state);
+      };
+
+      cordova.plugins.locationManager.setDelegate(delegate);
+
+      // Verify the platform supports transmitting as a beacon
+      cordova.plugins.locationManager.isAdvertisingAvailable()
+          .then(function(isSupported) {
+              if (isSupported) {
+                  console.log("Start advertising...");
+                  cordova.plugins.locationManager.startAdvertising(beaconRegion)
+                      .fail(conole.error)
+                      .done();
+              } else {
+                  console.log("Advertising not supported");
+              }
+          })
+          .fail(function(e) {
+              console.error(e);
+          })
+          .done();
+}
+
+function stopAdvertising() {
+  cordova.plugins.locationManager.stopAdvertising()
+      .fail(function(e) {
+          console.error(e);
+      })
+      .done();
+}
